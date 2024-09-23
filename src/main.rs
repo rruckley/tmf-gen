7532457074cd8733aa2e28d91ed78eb1d5dda7af
Ryan Ruckley <rruckley@gmail.com>
2024-09-23 16:01:42 +1000

    updates

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 
-use clap::{Parser,Subcommand};
-use log::{info,debug};
-use std::{fs::File, io::{Read, Write}, process::Output};
+use clap::Parser;
+use log::{info,error,debug};
+use std::{cell::Ref, fs::{self, File}, io::{Read, Write}, process::Output};
+use std::path::Path;
 use convert_case::{Case,Casing};
 use typify::{TypeSpace, TypeSpaceSettings};
 
-use openapiv3::OpenAPI;
+use openapiv3::{OpenAPI, ReferenceOr};
 
 #[derive(Parser,Debug)]
 struct Args {
@@ -14,6 +15,9 @@ struct Args {
 
     #[arg(long, help = "Output folder")]
     output: String,
+
+    #[arg(long, help = "TMF Number")]
+    tmf: String,
 }
 
 const SCHEMA_PRELUDE : &str = "\"$schema\": \"https://json-schema.org/draft/2020-12/schema\"";
@@ -30,53 +34,66 @@ fn main() -> Result<(),std::io::Error> {
 
     let file_name = args.file;
     let out_folder = args.output;
+    let tmf = args.tmf;
+
+    info!("Using input: {}",file_name);
 
     let mut file = File::open(file_name)?;
 
     let mut contents = String::new();
 
     file.read_to_string(&mut contents)?;
+    // dbg!(&contents);
+    let openapi: OpenAPI = serde_json::from_str(contents.as_str()).expect("Could not deserialise input");
 
-    let openapi : OpenAPI = serde_json::from_str(contents.as_str()).expect("Could not parse YAML");
+    let components = match openapi.components {
+        Some(c) => c,
+        None => {
+            error!("Could not find components");
+            return Ok(());
+        }
+    };
+    let root_folder = openapi.info.title;
+    info!("OpenAPI Title: {}",&root_folder);
+    let _result = fs::create_dir(format!("{}/{}",out_folder,tmf));
 
-    let components = openapi.components.expect("No components found!");
     let schemas = components.schemas;
 
-    let mod_title = openapi.info.title;
-    let mod_version = openapi.info.version;
-    info!("Processing module: {} v{}",mod_title,mod_version);
-    let out_schema = format!("{{\n\t{}",SCHEMA_PRELUDE);
-    for (name,schema) in schemas.iter() {
-        match schema.as_item() {
+    for (name,object) in schemas {
+        info!("Found schema: {}",name);
+        let mod_name = match name.split_once("_") {
             Some(s) => {
-                let json = serde_json::to_string(s)?;
-                out_schema.push_str(format!("\n\"{}\": {}\n"),name,json).as_str();
-                // let snake_name = name.to_case(Case::Snake);
-                // let snake_json = format!("{}.json",snake_name);
-                // let output_file = format!("{}/{}/{}/{}",out_folder,mod_title,mod_version,snake_json);
-                // info!("Creating schema: {}",output_file);
-                // let mut file = File::create(output_file)?;
-                // let out = format!("{{\n{},\n\"{}\": {}\n}}\n",SCHEMA_PRELUDE,name,json);
-                // file.write_all(out.as_bytes())?;      
+                String::from(s.0)
+            },
+            None => name.clone(),
+        }.to_case(Case::Snake);
+        let filename = format!("{}.rs",name.to_case(Case::Snake));
+        debug!("File Name: {}",filename);
 
-                // let mut type_space = TypeSpace::new(TypeSpaceSettings::default().with_struct_builder(true));
-                // let root_schema = serde_json::from_str::<schemars::schema::RootSchema>(&json).unwrap();
-                // type_space.add_root_schema(root_schema).unwrap();
+        let mod_path = format!("{}/{}/{}",out_folder,tmf,mod_name);
+        debug!("Mod path: {}",mod_path);
+        let _result = fs::create_dir(&mod_path);
 
-                // let contents = format!(
-                //          "{}\n{}",
-                //          "use serde::{Deserialize, Serialize};",
-                //          prettyplease::unparse(&syn::parse2::<syn::File>(type_space.to_stream()).unwrap())
-                // );
+        match object {
+            ReferenceOr::Reference { reference } => {
+                info!("Reference {}.{}",name,reference);
+            },
+            ReferenceOr::Item(i) => {
+                info!("Schema {}.schema",name);
+                // Since we are creating a file here, we need to add it to
+                let file_path = format!("{}/{}",mod_path,filename);
 
+                let mut file = match File::create_new(&file_path) {
+                    Ok(f) => f,
+                    Err(e) => File::open(&file_path).unwrap()
+                };
+
+                let file_contents = format!("//! {}\npub struct {} {{ }}\n",&name,&name);
+
+                let _result = file.write(file_contents.as_bytes())?;
             }
-            None => {}
         }
-
-        // Now create rust class
-        // // 
-        
-    }
+    }                                                                                                                                                                                                                                  
 
     Ok(())
 }
